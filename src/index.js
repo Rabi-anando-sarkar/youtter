2e5e6f6e6368186e93fb909192055b94c3c4ed17
Rabi-anando-sarkar <rabianandos2002@gmail.com>
2024-01-19 05:05:46 +0530

    utils codes

--- src/index.js
+++ src/index.js
@@ -6,23 +6,16 @@ dotenv.config({
     path: './env'
 })
 
-connectDB();
-
-/*
-;( async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("error",(error)=> {
-            console.log("ERROR : ",error);
-            throw error
-        })
-
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening on port ${process.env.PORT}`);
-        })
-    } catch (error) {
-        console.error("ERROR : ", error);
+connectDB()
+.then(() => {
+    app.on("error",(error) => {
+        console.log("Error => ",error);
         throw error
-    }
-})()
-*/
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(` Server is runnig at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGODB connection failed !!! ",err);
+})
