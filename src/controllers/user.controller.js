890484cf9dfbd80565309a74547070f4dfd8d05e
Rabi-anando-sarkar <rabianandos2002@gmail.com>
2024-02-11 03:02:18 +0530

    added new controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -185,7 +185,7 @@ const refreshAccessToken = asyncHandler(async (req,res) => {
     
         const options = {
             httpOnly : true,
-            secure : true
+            secure : true 
         }
     
         const {accessToken,newRefreshToken} = await generateAccessAndRefreshTokens(user._id)
@@ -207,4 +207,60 @@ const refreshAccessToken = asyncHandler(async (req,res) => {
 
 })
 
-export { registerUser,loginUser,logoutUser,refreshAccessToken }
+const changeCurrentPassword = asyncHandler(async(req,res) => {
+    const {oldPassword, newPassword} = req.body
+
+    const user = await User.findById(req.user?._id)
+
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+    if(!isPasswordCorrect) {
+        throw new ApiError(400, "Inavlid old password")
+    }
+
+    user.password = newPassword
+
+    await user.save({validateBeforeSave: false})
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, "Password Changed Succesfully"))
+})
+
+const getCurrentUser = asyncHandler(async(req,res) => {
+    return res
+    .status(200)
+    .json(200, req.user, "current user fetched succesfully")
+})
+
+const updateAccountDetails = asyncHandler(async(req,res) => {
+    const {fullName, email} = req.body;
+    if(!fullName || !email) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const user = User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                fullName,
+                email
+            }
+        },
+        {new: true}
+        ).select("-password")
+
+        return res
+        .status(200)
+        .json(new ApiResponse(200, user ,"Account Details updated Succesfully"))
+})
+
+export { 
+    registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails
+}
